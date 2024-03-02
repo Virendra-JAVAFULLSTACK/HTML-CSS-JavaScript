664c91c76c089317553e348013b7b2e199964f8c
Virendra-JAVAFULLSTACK <virendra12012000@gmail.com>
2024-03-02 22:58:26 +0530

    Done

---
+++ EMI Calculator/emi.js
@@ -0,0 +1,32 @@
+let loanAmount = document.getElementById("amount");
+let interestRate = document.getElementById("interest");
+let loanDuration = document.getElementById("loanTenure");
+let submit = document.getElementById("calculate");
+submit.addEventListener('click',(e)=>{
+    e.preventDefault();
+    calculateEMI();
+})
+function calculateEMI(){
+    let isYear = document.getElementById("year").chacked;
+    let isMonth = document.getElementById("month").checked;
+    let noOfMonths = 0;
+    if(isYear=="" && isMonth==""){
+        alert("Please select loan tenure type-- Month or Year");
+    }else{
+        if(isYear==true){
+            noOfMonths = loanDuration.value*12;
+        }else{
+            noOfMonths = loanDuration.value;
+        }
+        let r = parseFloat(interestRate.value)/12/100;
+        let p = loanAmount.value;
+        let n = noOfMonths;
+        let EMI = (p*r*Math.pow((1+r),n))/(Math.pow((1+r),n)-1);
+        let totalInterest = (EMI * n) - p;
+        let totalPayment = totalInterest + parseFloat(p);
+        document.getElementById("emi").innerText = "Rs." + Math.round(EMI);
+        document.getElementById("totalInterest").innerText = "Rs." + Math.round(totalInterest);
+        document.getElementById("totalPayment").innerText = "Rs." + Math.round(totalPayment);
+    }
+}
+
